31621f3ab9a854c6a2265f06a3d971cf4dd3c4bd
Kirill Franzhev <kiria-b2@mail.ru>
2016-06-15 18:36:55 +0300

    Project created

---
+++ app/src/main/java/com/icecodersteam/kiria/postpigeon/Dialogs.java
@@ -0,0 +1,13 @@
+package com.icecodersteam.kiria.postpigeon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dialogs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogs);
+    }
+}
